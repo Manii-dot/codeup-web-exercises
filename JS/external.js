10c2fd85ff0cb574eca1803bfb7ea4cf5db79750
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-02-02 12:26:06 -0600

    feat: JavaScrip with HTML exercices

---
+++ JS/external.js
@@ -0,0 +1,14 @@
+console.log("Hello from external JavaScript");
+
+//Alert
+alert('Welcome to my Website!');
+
+//Prompt
+
+var favoriteColor = prompt("What is your favorite color?");
+console.log("Great, " + favoriteColor + " is my favorite color too!");
+
+//Fun exercise
+
+var number = prompt('What is your number Gir?');
+console.log("Alrighty, that is what I will play tonight!");
