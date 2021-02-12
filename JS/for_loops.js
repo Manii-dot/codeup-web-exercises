16c0d5621ac1cc20d8d9979865f5a5f103b2fc23
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-02-12 17:00:44 -0600

    feat: completed 3 exercises for the Loops.js

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,10 +1,45 @@
-function showMultiplicationTable(x){
-for(var i = 1; i <= 10; i++){
-    var result = i * x;
-    console.log((result));
+// Exercise 2
+
+// function showMultiplicationTable(x){
+// for(var i = 1; i <= 10; i++){
+//     var result = i * x;
+//     console.log((x) + "*" + (i) + "=" + (result));
+//     }
+// }
+//
+// showMultiplicationTable(7);
+
+
+//Exercise 3
+// Use a for loop and the code from the previous lessons to generate 10 random
+// numbers between 20 and 200 and output to the console whether each number is odd
+//or even.
+//
+// function showMultiplicationTable(){
+//     for(var i = 1; i <= 10; i++){
+//         var random = Math.floor((Math.random() * (201-20) + 20));
+//         if(random % 2 == true){
+//             console.log( random + " is an odd");
+//         }else{
+//             console.log(random + " is an even");
+//         }
+//     }
+// }
+// showMultiplicationTable();
+//
+// Create a for loop that uses console.log to create the output shown below.
+
+
+    for (var i = 1; i <= 9; i++) {
+        var output = "";
+        for (var j = 1; j <= i; j++){
+            output = output + i.toString();
     }
+        console.log(output)
 }
 
-showMultiplicationTable(7);
+    //Create a for loop that uses console.log to create the output shown below.
 
+for(var i = 1; i <= 20; i++){
 
+}
