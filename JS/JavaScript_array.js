4bc4c3c80a83ac097d4d701bcf3ce652ec3f846b
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-03-27 14:43:03 -0500

    feat: fun array exercise

---
+++ JS/JavaScript_array.js
@@ -0,0 +1,49 @@
+/*
+1- Write a JavaScript function to check whether an `input` is an array or not.
+ */
+// function IsArray(input){
+//     return (Array.isArray(input));
+// }
+// console.log(IsArray('w3resource'));
+
+/*
+2- Write a JavaScript function to clone an array.
+ */
+// function cloneArray(array){
+//     return array.slice(0);
+// }
+// console.log([1, 2, 4, 0]);
+
+/*
+3- Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array.
+ */
+// function first(n){
+//     return n[0];
+// }
+// console.log(first(['string', 'yes', 3]));
+
+/*
+4-
+ */
+// function last(n){
+//     return n[n.length -1];
+// }
+// console.log(last(['string', 'yes', 3]));
+
+/*
+5-Write a simple JavaScript program to join all elements of the following array into a string.
+ */
+// var myColor = ["Red", "Green", "White", "Black"];
+// function joining(array){
+//     return array.join(" ");
+// }
+// console.log(joining(myColor));
+
+/*
+6-Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers. For example if you accept 025468 the output should be 0-254-6-8.
+ */
+function program(num){
+    if(){
+
+    }
+}
