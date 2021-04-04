ffc64e2bc055a91f2b1e232cd65831b05580c738
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-03 20:28:15 -0500

    Feat: Array and Object exercises practice

--- JS/JavaScript_array.js
+++ JS/JavaScript_array.js
@@ -97,7 +97,7 @@
 
 // Assessment Prep
 //
-// function totalPets(array){
+// function totalPet(array)
 //     var sum = 0;
 //     for(var = i; i < petsArray.length; i++ ){
 //         sum = sum + petsArray[i].numPets;
@@ -114,6 +114,116 @@
 //
 // console.log(10 % 3);
 
-var array = ['John', 'Paul', 'George', 'Ringo', 'Santana'];
+// var array = ['John', 'Paul', 'George', 'Ringo', 'Santana'];
+//
+// console.log(array[array.length - 3]);
 
-console.log(array[array.length - 3]);
+//----------------------------------------------------------------------------------------
+
+// let myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+// function myAlphabetLength(){
+//     console.log(myAlphabet.length);
+//     if(myAlphabet.length < 4){
+//         return ("Oops it is not greater than 4!!");
+//     }
+// }
+//
+// myAlphabetLength();
+//
+// function checkFunc(string, boolean){
+//
+// }
+// console.log(checkFunc("a", true));
+
+// var Planets = ["Earth", "Jupiter", "Mercury", "Mars", "Venus"];
+// for( var i =0; i < Planets.length; i++) {
+//     console.log("Planet " + Planets[i]+ " is found at the index of "+ i);
+// }
+// console.log(Planets)
+//
+//
+// var wowDatatypes = [ " yes", 3, !false, "ok.", 4];
+// for( var i =0; i < wowDatatypes.length; i++) {
+//     console.log(wowDatatypes[i]+" is a " + typeof wowDatatypes[i] + " data that is found at the index of " + i);
+// }
+//
+//
+// let myArr = [ 1, 2, 'One', true];
+// console.log(myArr[0]);
+// console.log(myArr[1]);
+// console.log(myArr[2]);
+// console.log(myArr[3]);
+
+// Write a function named totalPets that accepts an array of objects where each object represents a person, and has a 'pets' property for their owned pets. The function should return the sum of every object's numPets property.
+// totalPets([{name: 'Fernando Mendoza', numPets: 1},{name: 'Douglas Hirsh', numPets: 8}, {name: 'Kenneth Howell', numPets: 3}]) //11
+
+var petsArray = [{
+    name: "Fernando Mendoza",
+    numPets: 1
+},{
+    name: "Douglas Hirsh",
+    numPets: 8
+},{
+    name: "Kenneth Howell",
+    numPets: 3
+}]
+// our game plan:
+//     * write the function first
+// * one parameter
+// * deal with the array
+// ** extract what we need (RV) AND we probably need a LOOP [FD]
+// *** need a loop? that's what a for loop is FOR!
+//     *** extraction: extract each individual numPets in this loop
+// ** how about a BUCKET [a place to store our running sum]
+//
+// our function:
+
+// function totalPets(petsArray){
+//
+//     var sum = 0;
+//     for(var i = 0; i < petsArray.length; i++){
+//         sum = sum + petsArray[i].numPets;
+//         console.log("var 'sum' is currently " + sum + " "+ petsArray[i].name) //uncomment if you want to track the running total of sum
+//     }
+//     return sum;
+// }
+// totalPets(petsArray);
+// more drills:
+// Write a function named countPosi that accepts an array of numbers and returns the number of positive numbers in the array.
+// countPosi([-2, 7, 4, -9, 1]) //3
+// countPosi([3, 6, 9, -1]) //3
+// countPosi([10]) //1
+
+// function countPosi(arr){
+//     // for(var i = 0; i>arr.length; i++ ){
+//     //
+//     // }
+//     var posArr = arr.filter(function(num){
+//         return num > -1;
+//     })
+//     return posArr.length;
+// }
+// console.log(countPosi([3, 6, 9, -1, 8, -12, 200]));
+
+// Write a function named printRecipe that accepts a recipe object. A recipe object will have properties of title (a string), servings (a number), and ingredients (an array of strings). printRecipe will console.log each property in this format:
+//     Example object:
+     var caldoRecipe = {name: "Caldo con Pollo", servings: 4, ingredients: ["whole chicken", "carrots", "cabbage", "potatoes", "jalapeno", "cilantro", "salt", "pepper", "cumin", "onion powder", "garlic powder"]}
+// Expected look in console from running printRecipe(caldoRecipe):
+// Caldo con Pollo
+// Serves: 4
+// Required ingredients: whole chicken, carrots, cabbage, potatoes, jalapeno, cilantro, salt, pepper, cumin, onion powder, garlic powder
+
+function printRecipe(x){
+    for(var i = 0; i < caldoRecipe.length; i++){
+        return(caldoRecipe[i]);
+    }
+}
+console.log(printRecipe(caldoRecipe));
+
+
+
+
+// Write a function named convertAddressToObject that accepts a string that contains a street number (as a string) and street name separated by a space characters, and returns an object with properties streetNumber and streetName.
+// convertAddressToObject('8626 Sunny Oaks') // {streetNumber: '8646', streetName: 'Sunny Oaks'}
+// convertAddressToObject('5408 Villa Nueva') // {streetNumber: '5408', streetName: 'Villa Nueva'}
+function convertAddressToObject
