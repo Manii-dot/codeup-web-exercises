f72679a8404ce43d88b8db33d1023d99942d4b1b
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-06 22:03:41 -0500

    feat: started jquery events lecture

--- JS/jquery_exercises.js
+++ JS/jquery_exercises.js
@@ -1,1 +1,24 @@
-$('.important, p').css('background-color', 'red');
+// $('.important, p').css('background-color', 'red');
+// $('.codeup').css('border', '1px solid red');
+
+// var contents = $('#yes1').html();
+// alert(contents);
+//---------------Mouse Events-------------------------------//
+
+// $('#coding').click(function(){
+//     // alert('Hmmmm, example page');
+//     $(this).css('Background-color', '#FF0')
+// });
+
+$('#theList').hover(
+    function() {
+        $(this).css('background-color', 'red');
+    },
+    function() {
+        $(this).css('background-color', '#FFF');
+    }
+);
+
+$('#codeup').dblclick(function(e) {
+    alert('h1 with the id of "codeup" was double clicked!');
+});
