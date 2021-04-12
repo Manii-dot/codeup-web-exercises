2a8c19a46a2cef2c6840aa14be1b0f0976b3ab8f
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-12 13:52:01 -0500

    feat:starting on the open weather

--- JS/keys.js
+++ JS/keys.js
@@ -1,1 +1,2 @@
-var mapboxToken = "pk.eyJ1IjoibWFudXUxMDEiLCJhIjoiY2tuYWpwdXlxMWk5cjJxbjFtN2VkMmpmMyJ9.qHfbONMOOjG0yzfUcEgHMA";
+// var mapboxToken = "pk.eyJ1IjoibWFudXUxMDEiLCJhIjoiY2tuYWpwdXlxMWk5cjJxbjFtN2VkMmpmMyJ9.qHfbONMOOjG0yzfUcEgHMA";
+var openWeatherKey = "91c1d0e4e6ad5ba477f27aa09d5d56d5";
