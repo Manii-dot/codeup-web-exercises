eeaaf490ad24845c906485d87e70630af68f073b
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-13 11:53:55 -0500

    feat: pulling the current city lived in

--- weather_map_ directory /weather_map.js
+++ weather_map_ directory /weather_map.js
@@ -1,2 +1,14 @@
 console.log("weather map");
 
+/*
+
+ */
+
+$.get("https://api.openweathermap.org/data/2.5/weather", {
+    q: "Killeen, US",
+    appid: openWeatherKey,
+    units:"imperial"
+}).done(function(results){
+console.log(results)
+});
+
