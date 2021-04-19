1ae41ca341cff2b35a2a32682543647d13eb395d
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-19 15:03:58 -0500

    feat: Dom and Bom assessment added/ Rest api lecture started

--- JS/promises.js
+++ JS/promises.js
@@ -1,5 +1,5 @@
 console.log('Yes promised');
-
+                                        // missed the event part of the url
 var githubapi = fetch('https://api.github.com/users/Manii-dot', {headers: {'Authorization': gitHubToken}})
     .then((response)=>{
         console.log(response)
@@ -9,3 +9,24 @@ var githubapi = fetch('https://api.github.com/users/Manii-dot', {headers: {'Auth
     console.log("the last commit was: ", events.updated_at))
 ;
 
+// Another way to do this, Dauglas way that is_____
+
+function getLastpushDates(githubUsername){
+    let url = `https://api.github.com/users/${githubUsername}/events`;
+    let githubRsponse = fetch(url,{headers:{'Authorization': `token${gitHubToken}`}});
+    githubRsponse
+        .then((response)=>{
+            return response.json();
+        })
+        .then((githubEventData)=>{
+            for(let githubEvent of githubEventData){
+                if(githubEvent.type === "PushEvent"){
+                    console.log(githubEvent.created_at);
+                    return new Date(githubEvent.created_at)
+                }
+            }
+        })
+}
+
+getLastpushDates("Manii-dot")
+    .then((date)=>console.log(date));
