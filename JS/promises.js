7258bab343b0d74ec796909711dcb35d95ed1ab8
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-04-19 14:15:05 -0500

    feat:first part of exercise is complete

--- JS/promises.js
+++ JS/promises.js
@@ -1,4 +1,11 @@
 console.log('Yes promised');
 
-var githubapi = fetch(url, {headers: {'Authorization': gitHubToken}});
+var githubapi = fetch('https://api.github.com/users/Manii-dot', {headers: {'Authorization': gitHubToken}})
+    .then((response)=>{
+        console.log(response)
+        return response.json()
+    })
+    .then(events =>
+    console.log("the last commit was: ", events.updated_at))
+;
 
