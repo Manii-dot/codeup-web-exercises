a0a83162332d57807c381109e6ed3f42d23f8cd2
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-07-21 22:30:01 -0500

    feat:JavaScript Validation

---
+++ simple api/script.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    const inpObj = document.getElementById("id1");
+    if (!inpObj.checkValidity()) {
+        document.getElementById("demo").innerHTML = inpObj.validationMessage;
+    } else {
+        document.getElementById("demo").innerHTML = "Input OK";
+    }
+}
+
+const x = document.getElementById("demo");
+
+function getLocation() {
+    try {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } catch {
+        x.innerHTML = err;
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
