434c43b7eb59a83162b6e48577b7b5a8e9688798
Manii-Dot <emmanuel.stephen68@gmail.com>
2021-07-24 23:09:08 -0400

    sophicated answer

--- coding_exercise/script.js
+++ coding_exercise/script.js
@@ -1,16 +1,27 @@
 // Leet code problem easy
 // How to do a simple reverse interger 
 
-function reverse(x) {
-    return (
-    parseFloat(
-      x
-        .toString()
-        .split('')
-        .reverse()
-        .join('')
-    ) * Math.sign(x)
-  )               
-};
+// function reverse(x) {
+//     return (
+//     parseFloat(
+//       x
+//         .toString()
+//         .split('')
+//         .reverse()
+//         .join('')
+//     ) * Math.sign(x)
+//   )               
+// };
 
 console.log(reverse(774883993));
+
+
+// although the solution works above but for some reason the answer was wrong regardless. 
+
+ function reverse(x) {
+  
+    let xrev = parseInt(Math.abs(x).toString().split('').reverse().join(''));
+    
+    return xrev > Math.pow(2,31) - 1 ? 0 : x > 0 ? xrev : xrev * -1;
+    
+  };
