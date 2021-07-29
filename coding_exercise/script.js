1d7c61d9756633bac8bc623c19964c726dd785b6
Manii-Dot <emmanuel.stephen68@gmail.com>
2021-07-28 23:08:24 -0400

    remover Char

--- coding_exercise/script.js
+++ coding_exercise/script.js
@@ -56,5 +56,8 @@ It's pretty straightforward. Your goal is to create a function that removes the 
 
 function removeChar(str){
     //You got this!
-   
-   };
+    str1 = str.split('');
+    str1.shift();
+    str1.pop();
+    return str1.join('');
+};
