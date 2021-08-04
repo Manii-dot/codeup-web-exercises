163b0b21fccd879a983fbc73ee1fbab69609496d
Manii-Dot <emmanuel.stephen68@gmail.com>
2021-08-03 23:01:03 -0400

    more katas

--- coding_exercise/script.js
+++ coding_exercise/script.js
@@ -101,3 +101,18 @@ function divisors(integer) {
 function isValidWalk(walk) {
   //insert brilliant code here
 }
+
+
+/*
+Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+*/
+
+function spEng(sentence){
+  //write your code here
+  }
